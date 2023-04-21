36137ab3d26a935799de00175a9beefcdd3e4dc3
nick thomas <nichoth@gmail.com>
2023-04-21 16:23:00 -0700

    better test

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,12 @@ import stringify from 'json-stable-stringify'
 import { Implementation } from '@oddjs/odd/components/crypto/implementation'
 type KeyStore = Implementation['keystore']
 
-export async function create (keystore:KeyStore, obj:object):Promise<object> {
+interface SignedRequest {
+    signature: string,
+    [key: string]: any
+}
+
+export async function create (keystore:KeyStore, obj:object):Promise<SignedRequest> {
     const sig = toString(await sign(keystore, stringify(obj)))
     const req = { signature: sig, ...obj }
     return req
--- test/index.ts
+++ test/index.ts
@@ -1,108 +1,25 @@
 import * as odd from '@oddjs/odd'
-import * as BrowserCrypto from '@oddjs/odd/components/crypto/implementation/browser'
+import { components } from '@ssc-hermes/node-components'
 import { test } from 'tapzero'
 import { create } from '@ssc-hermes/request'
 
-// Components
-const crypto = createCryptoComponent()
-const depot = createDepotComponent()
-const storage = odd.storage.memory()
-const config = {
-    namespace: 'public-data-viewer',
-    debug: true
-}
+let program
 
-const fissionComponents = await odd.compositions.fission({
-    ...config,
-    // @ts-ignore
-    crypto,
-    storage,
+test('setup', async t => {
+    program = await odd.assemble({
+        namespace: { creator: 'test', name: 'testing' },
+        debug: true
+    }, components)
+
+    t.ok(program, 'create a program')
 })
 
-const componentsWithCustomDepot = {
-    ...fissionComponents,
-    depot
-}
-
-const program = await odd.assemble(
-    config,
-    // @ts-ignore
-    componentsWithCustomDepot
-)
-
-const { keystore } = program.components.crypto
-
+let req
 test('create request', async t => {
-    const req = await create(keystore, { type: 'test', value: 'wooo' })
-    console.log('**req**', req)
+    const { keystore } = program.components.crypto
+
+    req = await create(keystore, { type: 'test', value: 'wooo' })
     t.ok(req, 'request was created')
+    t.equal(typeof req.signature, 'string', 'should have a signature')
+    t.equal(req.type, 'test', 'should have the properties we passed in')
 })
-
-function createCryptoComponent () {
-    const {
-        aes,
-        did,
-        hash,
-        misc,
-        rsa,
-    } = BrowserCrypto
-
-    return {
-        aes,
-        did,
-        hash,
-        misc,
-        rsa,
-
-        // We're avoiding having to implement all of this,
-        // because we're not using it anyway.
-        // ---
-        // One way to actually implement this would be to
-        // set up the keystore-idb library to use an in-memory
-        // store instead of indexedDB. There's an example in
-        // the Webnative tests.
-        keystore: {
-            clearStore: boom,
-            decrypt: boom,
-            exportSymmKey: boom,
-            getAlgorithm: boom,
-            getUcanAlgorithm: boom,
-            importSymmKey: boom,
-            keyExists: boom,
-            publicExchangeKey: boom,
-            publicWriteKey: boom,
-            sign: boom,
-        }
-    }
-}
-
-function createDepotComponent () {
-    const ipfsGateway = 'https://ipfs.runfission.com'
-
-    function ipfs (path) {
-        return fetch(`${ipfsGateway}${path}`)
-            .then(r => r.arrayBuffer())
-            .then(r => new Uint8Array(r))
-    }
-
-    return {
-        // Get the data behind a CID
-        getBlock: async cid => {
-            return ipfs(`/api/v0/block/get?arg=${cid.toString()}`)
-        },
-        getUnixFile: async cid => {
-            return ipfs(`/api/v0/cat?arg=${cid.toString()}`)
-        },
-
-        // We're avoiding having to implement all of this,
-        // because we're not using it anyway.
-        getUnixDirectory: boom,
-        putBlock: boom,
-        putChunked: boom,
-        size: boom,
-    }
-}
-
-function boom () {
-    throw new Error('Method not implemented')
-}
